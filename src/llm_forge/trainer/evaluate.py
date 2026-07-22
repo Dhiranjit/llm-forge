b81c874bcdc2855933ef59101dc754f9a7fc02e5
Dhiranjit <dhiranjitdaimary@gmail.com>
2026-07-22 17:00:51 +0530

    Move evalute_validation_loss to evaluate.py

---
+++ src/llm_forge/trainer/evaluate.py
@@ -0,0 +1,26 @@
+import torch
+import torch.distributed as dist
+
+
+
+
+@torch.no_grad()
+def evaluate_validation_loss(model, val_loader, eval_iters):
+    model.eval()
+
+    losses = torch.zeros(eval_iters, device=val_loader.device)
+    val_loader.reset()
+
+    with torch.autocast(device_type="cuda", dtype=torch.float16):
+        for k in range(eval_iters):
+            x, y = val_loader.next_batch()
+            _, loss = model(x, y)
+            losses[k] = loss
+
+    mean_loss = losses.mean()
+
+    if dist.is_initialized():
+        dist.all_reduce(mean_loss, op=dist.ReduceOp.AVG)
+
+    model.train()
+    return mean_loss.item()
